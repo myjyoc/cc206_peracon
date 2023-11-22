e45572528d3e207cec1f4f96078dc645301fed30
Mary Joy Consular <maryjoy.consular@wvsu.edu.ph>
2023-11-22 17:33:50 +0800

    Update learn_page.dart

--- lib/features/learn_page.dart
+++ lib/features/learn_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LearnPage extends StatelessWidget {
-  const LearnPage({Key? key});
+  const LearnPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +10,12 @@ class LearnPage extends StatelessWidget {
         appBar: AppBar(
           elevation: 0.0,
           backgroundColor: Colors.transparent,
-          title: Row(
+          title: Container(
+          child: Row(
             children: [
               Expanded(
                 child: Container(
+                  
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(30.0),
                     border: Border.all(color: Colors.grey),
@@ -43,206 +45,128 @@ class LearnPage extends StatelessWidget {
             ],
           ),
         ),
+        ),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              // Recommended for You and See All
+              // Headline News Section
               Container(
-                padding: EdgeInsets.all(16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                margin: EdgeInsets.all(16.0),
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
                   children: [
-                    Text(
-                      'Recommended for You',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                      ),
+                    Image.network(
+                      'https://cf.ltkcdn.net/family/images/orig/277805-1600x1066-filipino-culture-traditions.jpg',
+                      height: 210.0,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
                     ),
-                    TextButton(
-                      onPressed: () {
-                        // Handle See All action
-                      },
+                    Container(
+                      color: Colors.black.withOpacity(0.5),
                       child: Text(
-                        'See All',
+                        'Empowering the Future: Bridging the Gap with Financial Education for Filipino Millennials',
                         style: TextStyle(
-                          color: Colors.blue,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontFamily: 'Open Sans',
                         ),
                       ),
                     ),
                   ],
-                ),
-              ),
-
-              // Headline News Section
-              GestureDetector(
-                onTap: () {
-                  // Handle click on the headline news
-                },
-                child: Container(
-                  margin: EdgeInsets.all(16.0),
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      Image.network(
-                        'https://cf.ltkcdn.net/family/images/orig/277805-1600x1066-filipino-culture-traditions.jpg',
-                        height: 210.0,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ),
-                      Container(
-                        color: Colors.black.withOpacity(0.5),
-                        child: Text(
-                          'Empowering the Future: Bridging the Gap with Financial Education for Filipino Millennials',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
                 ),
               ),
 
               // Category Buttons Section
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Container(
+                   margin: EdgeInsets.only(left: 8.0, right: 8.0),
+                   child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+                      ),
+                      child: Text('Budgeting'),
+                    ),
+                  ),
+                  ), 
+                  SizedBox(width: 10.0),
+                  Expanded(
+                    child: Container(
+                    margin: EdgeInsets.only(left: 8.0, right: 8.0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+                     ),
+                      child: Text('Debt Management'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+              // Other News Section
+              // News 1
               Container(
-                height: 50.0,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
+                margin: EdgeInsets.all(16.0),
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
                   children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        // Handle category button click
-                      },
-                      child: Text('Category 1'),
+                    Image.network(
+                      'https://media.philstar.com/images/the-philippine-star/lifestyle/arts-and-culture/20170528/PHILSTAR/BUSINESS%20FEATURES/usual/financial.jpg',
+                      height: 180.0,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
                     ),
-                    SizedBox(width: 10.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Handle category button click
-                      },
-                      child: Text('Category 2'),
-                    ),
-                    SizedBox(width: 10.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Handle category button click
-                      },
-                      child: Text('Category 3'),
-                    ),
-                    SizedBox(width: 10.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Handle category button click
-                      },
-                      child: Text('Category 4'),
-                    ),
-                    SizedBox(width: 10.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Handle category button click
-                      },
-                      child: Text('Category 5'),
+                    Container(
+                      color: Colors.black.withOpacity(0.5),
+                      child: Text(
+                        'Filipinos Financial Literacy: What Numbers Say and Why It Matters',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontFamily: 'Open Sans', 
+                        ),
+                      ),
                     ),
                   ],
                 ),
               ),
 
-              // Other News Section
-              // News 1
-              GestureDetector(
-                onTap: () {
-                  // Handle click on News 1
-                },
-                child: Container(
-                  margin: EdgeInsets.all(16.0),
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      Image.network(
-                        'https://media.philstar.com/images/the-philippine-star/lifestyle/arts-and-culture/20170528/PHILSTAR/BUSINESS%20FEATURES/usual/financial.jpg',
-                        height: 180.0,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ),
-                      Container(
-                        color: Colors.black.withOpacity(0.5),
-                        child: Text(
-                          'Filipinos Financial Literacy: What Numbers Say and Why It Matters',
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-
               // News 2
-              GestureDetector(
-                onTap: () {
-                  // Handle click on News 2
-                },
-                child: Container(
-                  margin: EdgeInsets.all(16.0),
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      Image.network(
-                        'https://media.philstar.com/photos/2023/05/22/1_2023-05-22_15-11-18276_thumbnail.jpg',
-                        height: 180.0,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ),
-                      Container(
-                        color: Colors.black.withOpacity(0.5),
-                        child: Text(
-                          'Empowering the Future: Educators Focus on Instilling Financial Education Among Youth',
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
+              Container(
+                margin: EdgeInsets.all(16.0),
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Image.network(
+                      'https://media.philstar.com/photos/2023/05/22/1_2023-05-22_15-11-18276_thumbnail.jpg',
+                      height: 180.0,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                    Container(
+                      color: Colors.black.withOpacity(0.5),
+                      child: Text(
+                        'Empowering the Future: Educators Focus on Instilling Financial Education Among Youth',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontFamily: 'Open Sans', 
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.book),
-              label: 'Learn',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.question_answer),
-              label: 'Ask',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.analytics),
-              label: 'Analytics',
-            ),
-          ],
-          selectedItemColor: Colors.blue,
-          unselectedItemColor: Colors.grey,
-          currentIndex: 1, // The index of the current tab (Learn in this case)
-          onTap: (int index) {
-            // Handle navigation to different tabs
-            // You can use a Navigator or any other navigation logic here
-          },
         ),
       ),
     );
